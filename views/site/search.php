bd927a9b5e69062dbd58b8218237bcc62b7c6d81
sbbakaev <sbbakaev@gmail.com>
2017-04-16 21:26:33 +0300

    change: links destination on search page.

--- views/site/search.php
+++ views/site/search.php
@@ -2,6 +2,7 @@
 
 /* @var $this yii\web\View */
 use yii\widgets\LinkPager;
+use yii\helpers\Url;
 
 $this->title = 'My Yii Application';
 ?>
@@ -27,15 +28,14 @@ $this->title = 'My Yii Application';
     <div class="row highlight">
         <div class="col-md-12">
             <div class="row">
-                <div class="col-md-2">
-                    <a href=<?php echo '"' . $repo['html_url'] . '">' . $repo['name'] ?></a>
+                <div class="col-md-5">
+                    View repo detail: <a href=<?php echo '"' . Url::to(['site/repo', 'id' => $repo['full_name']]) . '">' . $repo['name'] ?></a>
                 </div>
-                <div class="col-md-offset-1 col-md-5">
+                <div class="col-md-3">
                     <a href=<?php echo '"' . $repo['owner']['blog'] . '">' . $repo['owner']['blog'] ?></a>
                 </div>
-                <div class="col-md-offset-1 col-md-2">
-                    <!--                    --><?php //var_dump($repo['owner']) ?>
-                    <a href=<?php echo '"' . $repo['owner']['html_url'] . '">' . $repo['owner']['login'] ?></a>
+                <div class="col-md-4">
+                    View user detail: <a href=<?php echo '"' . Url::to(['site/user', 'id' => $repo['owner']['login']]) . '">' . $repo['owner']['login'] ?></a>
                 </div>
             </div>
             <div class="row">
